d4f27ceb23a261b0f847457d05828348d343982a
EternalKnight002 <resoamankumar@gmail.com>
2025-11-28 15:24:54 +0530

    re start

--- src/middleware/roleGuard.js
+++
@@ -1,9 +0,0 @@
-// src/middleware/roleGuard.js
-export const roleGuard = (...allowedRoles) => {
-  return (req, res, next) => {
-    if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ error: "Forbidden: insufficient role" });
-    }
-    next();
-  };
-};
